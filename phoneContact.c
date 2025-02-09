0bc90bcbf580cd1ced495f88fc9edf53221297ee
hsnali <hassanali7303@gmail.com>
2025-02-10 01:15:42 +0330

    complited main structer

--- phoneContact.c
+++ phoneContact.c
@@ -14,15 +14,25 @@ struct contact {
 
 int contactCount = 0 ;
 
-void addContact() {}
+void addContact() {
+    printf("addContact\n");
+}
 
-void listContacts() {}
+void listContacts() {
+    printf("listContacts\n");
+}
 
-void searchContact() {}
+void searchContact() {
+    printf("searchContact\n");
+}
 
-void deleteContact() {}
+void deleteContact() {
+    printf("deleteContact\n");
+}
 
-void saveContacts() {}
+void saveContacts() {
+    printf("saveContacts\n");
+}
 
 void loadContacts() {}
 
@@ -30,13 +40,27 @@ int main() {
     loadContacts();
     int choice;
 
-        printf("1. Add contact \n");
-        printf("2. Display contact list \n");
-        printf("3. Contact search \n");
-        printf("4. Delete contact \n");
-        printf("5. Save and exit \n");
-        printf("Your choice:");
-        scanf("%d",&choice);
+        do{
+            printf("\n📞 **Contact Management** 📞\n");
+            printf("1. Add contact\n");
+            printf("2. Display contact list\n");
+            printf("3. Contact search\n");
+            printf("4. Delete contact\n");
+            printf("5. Save and exit\n");
+            printf("Your choice:");
+            scanf("%d", &choice);
+
+            switch(choice)
+            {
+                case 1: addContact(); break;       
+                case 2: listContacts(); break;     
+                case 3: searchContact(); break;    
+                case 4: deleteContact(); break;         
+                case 5: saveContacts(); printf("👋 Goodbye!\n"); break;   
+                default : printf("⛔ Invalid selection!\n"); break;   
+            }
+
+        }while(choice != 5);
 
     return 0;
 }
