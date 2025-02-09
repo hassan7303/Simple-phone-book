dba8b810eb8d76708d9a7b4a32801193f11cd149
hsnali <hassanali7303@gmail.com>
2025-02-10 00:48:29 +0330

    added structer for do it

---
+++ phoneContact.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CONTACTS 100;
+
+struct contact {
+    char name[50];
+    char phone[20];
+    char email[50];
+}
+
+struct Contacts contactCount[MAX_CONTACTS];
+
+int contactCount = 0 ;
+
+void addContact() {}
+
+void listContacts() {}
+
+void searchContact() {}
+
+void deleteContact() {}
+
+void saveContacts() {}
+
+void loadContacts() {}
+
+int main() {}
